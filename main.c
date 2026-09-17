bc1c1960e727a7ad79e8f8dac58aa995fecd57ad
HyeJun2412440 <gpwns11@sookmyung.ac.kr>
2026-09-17 16:30:14 +0900

    project04

--- main.c
+++ main.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 
-int main(void){
-    char c;
+int main(void)
+{
+    float a, b, i;
 
-    printf("Enter a character : ");
-    scanf("%c", &c);
+    printf("분자를 입력하세요: ");
+    scanf("%f", &a);
 
-    c = c + 1;
+    printf("분모를 입력하세요: ");
+    scanf("%f", &b);
 
-    printf("The next character is %c\n", c);
+    i = a / b;
+
+    printf("나누기의 결과는 %f 입니다.", i);
 
     return 0;
-
 }
